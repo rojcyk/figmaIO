90c1d8f2cae9e732430adb6bd6e90b7377d90015
rojcyk <denis@rojcyk.com>
2020-07-18 22:43:26 +0200

    Trying a new way how files are exported

--- src/ui.ts
+++ src/ui.ts
@@ -15,7 +15,7 @@ interface ExtendedEmitter extends EventEmitter {
 
 /* The whole logic changes based on whether we are running the emmiter in
  * the renderer or in the code side of the plugin. */
-function createInterface(renderer?: boolean): ExtendedEmitter {
+function createInterface(): ExtendedEmitter {
   const emitter = new EventEmitter() as ExtendedEmitter
 
   /*********************
@@ -27,17 +27,10 @@ function createInterface(renderer?: boolean): ExtendedEmitter {
     if (result && result.event) emitter.emit(result.event, result.data)
   }
 
-  if (renderer) {
-    /* If we in the the renderer, we are using standard JS window.onmessage function
-     * and we are parsing information that came from the figma event
-     * https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onmessage */
-    window.onmessage = (ev: any): void => receive(ev.data.pluginMessage)
-  } else {
-    /* But if we are in the code part of figma, we are using custom figma listener.
-     * and we are listening for data coming from window
-     * Refer to https://www.figma.com/plugin-docs/api/figma-ui/#onmessage for more information */
-    figma.ui.onmessage = (data): void => receive(data)
-  }
+  /* If we in the the renderer, we are using standard JS window.onmessage function
+    * and we are parsing information that came from the figma event
+    * https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onmessage */
+  window.onmessage = (ev: any): void => receive(ev.data.pluginMessage)
 
   /*********************
    * SEND LOGIC
@@ -51,16 +44,10 @@ function createInterface(renderer?: boolean): ExtendedEmitter {
 
     const postData = { event, data }
 
-    if (renderer) {
-      /* The same logic as with event listeners. if we are in the renderer, we are using
-       * standard window messaging.
-       * https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage */
-      window.parent.postMessage({ pluginMessage: postData }, '*')
-    } else {
-      /* And if we are in the code part of the plugin, we are using Figma post message 
-       * https://www.figma.com/plugin-docs/api/figma-ui/#postmessage */
-      figma.ui.postMessage(postData)
-    }
+    /* The same logic as with event listeners. if we are in the renderer, we are using
+      * standard window messaging.
+      * https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage */
+    window.parent.postMessage({ pluginMessage: postData }, '*')
   }
 
   /*********************
@@ -83,5 +70,6 @@ function createInterface(renderer?: boolean): ExtendedEmitter {
 // export const html = createInterface(true)
 // export const script = createInterface()
 
-export const html = isRenderer ? createInterface(true) : undefined
-export const script = isRenderer ? undefined : createInterface()
+// export const html = isRenderer ? createInterface(true) : undefined
+// export const script = isRenderer ? undefined : createInterface()
+ export default createInterface()
